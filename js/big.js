2f7d6b9f999477b2f12dfd075f2e3050a0adad54
chanho <cksgh901@naver.com>
2021-07-11 20:13:22 +0900

    07.11

---
+++ js/big.js
@@ -0,0 +1,12 @@
+
+
+    $(function(){
+        var swiper = new Swiper(".swiper-container", {
+            loop : true,
+            autoplay : {delay:3500},
+            navigation : {
+                nextEl : ".swiper-button-next",
+                prevEl : ".swiper-button-prev",
+            },
+        });
+    });
---
+++ js/index.js
@@ -0,0 +1,111 @@
+// $(function(){
+//     $(".header_mainMenu > li").mouseover(function(){
+//         $(this).find('.header_depth2').stop().slideDown(300);
+//     }) .mouseleave(function(){
+//          $(this).find('.header_depth2').stop().slideUp(300);
+//     });
+// });
+
+$(function(){ 
+    $(".header_mainMenu > ul > li >ul").hide
+        
+ $(".header_mainMenu >ul> li").mouseover(function(){
+    console.log(1)
+     $(this).find('.header_depth2').stop().slideDown(10)
+ });  
+ $(".header_mainMenu >ul> li").mouseleave(function(){
+    $(this).find('.header_depth2').stop().slideUp(10)
+ });
+ 
+    var swiper = new Swiper(".mySwiper", {
+        loop:true,
+        autoplay:{delay:3000},           
+        navigation: {
+             nextEl: ".swiper-button-next",
+             prevEl: ".swiper-button-prev",
+         },
+     });
+
+     $(".index_board_title").click(function(){
+        $('.index_board_title').removeClass('on');
+        $(this).addClass('on');         
+        if($(this).text()=='베스트셀러') {
+            $('#index_board_notice').show();
+            $('#index_board_data').hide();
+        } else {
+            $('#index_board_notice').hide();
+            $('#index_board_data').show();
+        }
+     });
+
+     //console.log($('.menu').position().left)
+ var q_left = $('.index_wrap').position().left+1360;
+    
+ var q_top = $('.index_wrap').position().top;
+
+ $(".mypage_ad").css({
+     'top': 1490,
+     'right': 90
+ });
+ 
+ $(window).scroll(function(){
+     console.log($(window).scrollTop());
+     var new_top = $(window).scrollTop()+137;
+     $(".mypage_ad").stop().animate({
+     'top':new_top
+     
+ },300);
+ })
+
+ $(".support_total").click(function(){
+    $(".support_totalDetail").show();
+    $(".support_orderDetail").show();
+    $(".support_shippingDetail").show();
+    $(".support_cancelDetail").show();
+    $(".support_memberDetail").show();
+});
+ 
+ $(".support_order").click(function(){
+    $(".support_orderDetail").show();
+    $(".support_totalDetail").hide();
+     $(".support_shippingDetail").hide();
+     $(".support_cancelDetail").hide();
+     $(".support_memberDetail").hide();
+ });
+
+ $(".support_shipping").click(function(){
+    $(".support_shippingDetail").show();
+    $(".support_totalDetail").hide();
+    $(".support_orderDetail").hide();
+    $(".support_cancelDetail").hide();
+    $(".support_memberDetail").hide();
+});
+$(".support_cancel").click(function(){
+    $(".support_cancelDetail").show();
+    $(".support_totalDetail").hide();
+    $(".support_orderDetail").hide();
+    $(".support_shippingDetail").hide();
+    $(".support_memberDetail").hide();
+});
+$(".support_member").click(function(){
+    $(".support_memberDetail").show();
+    $(".support_orderDetail").hide();
+    $(".support_cancelDetail").hide();
+    $(".support_shippingDetail").hide();
+    $(".support_totalDetail").show();
+   
+});
+
+//  support_t2 클래스 내임 따로 주기 
+// 클래스명 얼추 정리하기 
+
+$(".support_t2").hide();
+$(".support_t").click(function(){
+    $(this).parent().parent().find('.support_t2').slideToggle(10)
+});
+
+}); 
+
+ 
+
+
