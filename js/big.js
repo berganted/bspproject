85b9be8dc6150e7308738388453225f0b25a9d29
eunssol <eunssol01@gmail.com>
2021-07-16 09:21:26 +0900

    eunsol

--- js/big.js
+++ js/big.js
@@ -1,5 +1,5 @@
 
-
+//BigIdx 스와이프
     $(function(){
         var swiper = new Swiper(".swiper-container", {
             loop : true,
@@ -24,3 +24,40 @@
              if(tCount >0) $input.val(Number(tCount)-1);    //0보다 클 경우 -1
             }
         }
+
+        // 수량버튼증감
+        $(function() {
+            $('#decreaseQuantity').click(function(e) {
+              e.preventDefault();
+              var stat = $('#numberUpDown').text();
+              var num = parseInt(stat, 10);
+              num--;
+              if (num <= 0) {
+                alert('더이상 줄일수 없습니다.');
+                num = 1;
+              }
+              $('#numberUpDown').text(num);
+            });
+            $('#increaseQuantity').click(function(e) {
+              e.preventDefault();
+              var stat = $('#numberUpDown').text();
+              var num = parseInt(stat, 10);
+              num++;
+          
+              if (num > 100) {
+                alert('더이상 늘릴수 없습니다.');
+                num = 100;
+              }
+              $('#numberUpDown').text(num);
+            });
+          });
+            
+          // 전체선택해제     
+          function selectAll(selectAll)  {
+            const checkboxes 
+                 = document.getElementsByName('bestcheck');
+            
+            checkboxes.forEach((checkbox) => {
+              checkbox.checked = selectAll.checked;
+            })
+          }
